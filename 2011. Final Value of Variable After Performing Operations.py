e51019ce154bd500c12b3ff9e0a383cd1b826eb3
fnilvuwu <mamatmks45@gmail.com>
2024-03-04 17:14:00 +0800

    new file:   2011. Final Value of Variable After Performing Operations.py

---
+++ 2011. Final Value of Variable After Performing Operations.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for operation in operations:
+            if "++" in operation:
+                x += 1
+            else:
+                x -= 1
+        return x
