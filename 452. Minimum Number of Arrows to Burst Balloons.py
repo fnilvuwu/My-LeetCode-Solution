42d464e704b3e1f46fbd5e96456f1e02d9b57b4e
fnilvuwu <mamatmks45@gmail.com>
2024-03-19 02:14:20 +0800

    fix

--- 452. Minimum Number of Arrows to Burst Balloons.py
+++ 452. Minimum Number of Arrows to Burst Balloons.py
@@ -3,16 +3,14 @@ class Solution:
         if not points:
             return 0
         
-        points = sorted(points)
+        points.sort(key=lambda x: x[1])
         
-        arrows = 1
+        arrows = 1 
         prev_end = points[0][1]
         
         for balloon in points[1:]:
             if balloon[0] > prev_end:
                 arrows += 1
                 prev_end = balloon[1]
-            else:
-                prev_end = min(prev_end, balloon[1])  
-                
+        
         return arrows
